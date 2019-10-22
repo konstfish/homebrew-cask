d8d45d7548f174bf6b621e0e722371ac9396c906
David Fischer <david@konst.fish>
2019-10-22 14:41:20 +0200

    update coral vers

--- Casks/coral.rb
+++ Casks/coral.rb
@@ -1,5 +1,5 @@
 cask 'coral' do
-  version '0.0.1'
+  version '0.0.2'
   sha256 'ecdaa2579527929b2388e6b6db6a09f3b69b5db5855503a9d7e27da99cdcb29a'
 
   url "https://github.com/konstfish/Coral/releases/download/#{version}/Coral-#{version}.zip"
